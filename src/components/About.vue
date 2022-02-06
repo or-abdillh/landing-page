2da7503c9a291d263b1fd15269426c2e3021fb70
or abdillh <hans.abdillh05@gmail.com>
2022-02-06 11:56:03 +0800

    Fix bug

--- src/components/About.vue
+++ src/components/About.vue
@@ -2,7 +2,7 @@
 	<main id="about" class="py-10">
 		<h1 class="text-center pb-6 text-3xl md:text-4xl lg:text-5xl font-semibold text-indigo-500">Tentang</h1>
 		<section class="mt-10 md:flex md:gap-8 md:items-start xl:items-start">
-			<img class="md:mx-auto mb-5 md:w-3/12 lg:w-2/12" src="about.png" width="150" />
+			<img class="md:mx-auto mb-5 md:w-3/12 lg:w-2/12" src="/about.png" width="150" />
 			<p class="xl:px-6 lg:text-lg text-gray-600 xl:w-9/12 leading-thight">
 				<span class="text-indigo-700 font-medium">Diadakannya</span> kegiatan IT Gathering dimaksudkan untuk membangun rasa 
 				kekeluargaan antar pengurus dan anggota melalui berbagai kegiatan yang bersifat kelompok
