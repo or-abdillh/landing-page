29c1f7df2fd7e47341caea67916d2d7971114407
or abdillh <hans.abdillh05@gmail.com>
2022-02-07 15:53:44 +0800

    Update speakers

--- src/speakers.js
+++ src/speakers.js
@@ -2,7 +2,7 @@ export default [
 	{
 		name: 'Tesdiq Prigel Kaloka, M.Si',
 		job: 'Kepala prodi D3 TI Polihasnur dan pembina HIMA TI Polihasnur',
-		avatar: '/avatar.jpg'
+		avatar: 'https://dl.dropbox.com/s/a71h9jwiamy1fqs/images%20%2821%29-picsay.jpeg?dl=2'
 	},
 	{
 		name: 'Efendi Syaifullah',
