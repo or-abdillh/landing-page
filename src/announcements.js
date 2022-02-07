44d70ded13346b61039d62681417c48b18410d6c
or abdillh <hans.abdillh05@gmail.com>
2022-02-07 19:03:48 +0800

    update content

--- src/announcements.js
+++ src/announcements.js
@@ -3,7 +3,7 @@ export default [
 		title: 'Soisialisasi kegiatan IT Gathering HIMA TI Polihasnur 2022',
 		publishDate: 'Senin, 7 februari 2022',
 		content: `
-			Sehubungan dengan akan diadakannya kegiatan IT Gathering 2022, HIMA TI Polihasnur akan mengadakan sosialisasi kepada para peserta secara daring melalui media zoom atau gmeet pada hari <strong>Rabu, 9 februari 2022</strong>>. </br>
+			Sehubungan dengan akan diadakannya kegiatan IT Gathering 2022, HIMA TI Polihasnur akan mengadakan sosialisasi kepada para peserta secara daring melalui media zoom atau gmeet pada hari <strong>Rabu, 9 februari 2022</strong>. </br>
 			Untuk link zoom atau gmeet akan diinfokan lebih lanjut nantinya. 
 		`
 	}
