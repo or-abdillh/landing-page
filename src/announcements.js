a3c60d9a74dbf4f904acf9ab2e14113552f1c5a7
or abdillh <hans.abdillh05@gmail.com>
2022-02-26 12:20:34 +0800

    Update announcements content

--- src/announcements.js
+++ src/announcements.js
@@ -8,5 +8,13 @@ export default [
 			Sehubungan dengan akan diadakannya kegiatan IT Gathering 2022, HIMA TI Polihasnur akan mengadakan sosialisasi kepada para peserta secara daring melalui media zoom atau gmeet pada hari <strong>Rabu, 9 februari 2022</strong>. </br>
 			Untuk link zoom atau gmeet akan diinfokan lebih lanjut nantinya. 
 		`
+	},
+	{
+	  title: 'Twibbon IT Gatehring HIMA TI Polihasnur Tahun 2022',
+	  publishDate: 'Sabtu, 26 februari 2022',
+	  content: `
+	    Ayo semarakkan kegiatan IT Gathering tahun 2022 dengan ikut menggunakan twibbon official dan jangan lupa untuk mention dan share sebanyak - banyaknya. <br>
+	    Twibbon bisa didapatkan <a href="twb.nz/it-gathering-2022" target="_blank">disini</a>
+	  `
 	}
 ]
